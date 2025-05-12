8aaf40dcde5723a839b7188882dfa89a9397a901
Markus Johansson <markus.h.johansson@skane.se>
2025-05-12 08:44:30 +0200

    Api auth uses bonsai base

--- api/bonsai_api/auth.py
+++ api/bonsai_api/auth.py
@@ -3,11 +3,13 @@
 import random
 import string
 from datetime import datetime, timedelta
+from typing import Any
 
 from jose import jwt
 from passlib.context import CryptContext
 
 from .config import ALGORITHM, settings
+from bonsai_models.util import get_timestamp
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -28,13 +30,13 @@ def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
 
 
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
+def create_access_token(data: dict[str, Any], expires_delta: timedelta | None = None) -> str:
     """Create new access token."""
-    to_encode: dict = data.copy()
+    to_encode = data.copy()
     if expires_delta:
-        expire: datetime = datetime.utcnow() + expires_delta
+        expire: datetime = get_timestamp() + expires_delta
     else:
-        expire: datetime = datetime.utcnow() + timedelta(minutes=15)
+        expire: datetime = get_timestamp() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=ALGORITHM)
     return encoded_jwt
