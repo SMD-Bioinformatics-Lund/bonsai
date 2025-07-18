91764e84fa122546e0e848299dc84e7f6bff47bb
Ryan James Kennedy <ryanjameskennedy@icloud.com>
2025-07-18 21:59:46 +0200

    Fix routing path bugs

--- frontend/web/js/components/group-list.ts
+++ frontend/web/js/components/group-list.ts
@@ -6,8 +6,8 @@ function groupCardHTML(group: GroupInfo, isAdmin: boolean): string {
   return String.raw`
     <div class="col-sm-6 col-md-4 col-lg-auto py-2">
       <div class="card group-card position-relative">
-        ${isAdmin ? `<a class="d-inline-block badge bage-pill bg-secondary edit-button position-absolute top-0 start-100 translate-middle" role="button" href="/groups/edit/${group.group_id}"><i class="bi bi-pencil"></i></a>` : ""}
-        <a href="/groups/${group.group_id}" class="text-decoration-none text-dark">
+        ${isAdmin ? `<a class="d-inline-block badge bage-pill bg-secondary edit-button position-absolute top-0 start-100 translate-middle" role="button" href="${baseUrl}/groups/edit/${group.group_id}"><i class="bi bi-pencil"></i></a>` : ""}
+        <a href="${baseUrl}/groups/${group.group_id}" class="text-decoration-none text-dark">
           <div class="card-body">
             <h5 class="card-title">${group.display_name}</h5>
             <span class="text-muted text-uppercase fw-semibold n-samples">Samples: ${group.included_samples.length}</span>
@@ -69,7 +69,7 @@ export class GroupList extends HTMLElement {
     if (this.isAdmin) {
       html += String.raw`
         <div class="col-sm-6 col-md-4 col-lg-auto py-2">
-          <a class="card group-card position-relative text-center border-secondary h-100 d-flex align-items-center justify-content-center" href="/groups/edit">
+          <a class="card group-card position-relative text-center border-secondary h-100 d-flex align-items-center justify-content-center" href="${baseUrl}/groups/edit">
             <div class="rounded-circle bg-secondary text-white fw-bold d-flex align-items-center justify-content-center" style="width: 3.75rem; height: 3.75rem; font-size: 2.5rem; line-height: 0;">
               <i class="bi bi-plus-lg"></i>
             </div>
