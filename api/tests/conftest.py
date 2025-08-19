7113b3da7efdb677c43fa3d2630f157b4f6d1419
Markus Johansson <markus.johansson@me.com>
2025-08-19 14:47:40 +0200

    Added additional tests

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,5 +1,7 @@
 import json
 from contextlib import contextmanager
+import stat
+import textwrap
 from typing import Generator
 
 import pytest
@@ -11,6 +13,7 @@ from bonsai_api.main import app
 from bonsai_api.models.sample import PipelineResult, SampleInDatabase
 from fastapi.testclient import TestClient
 from mongomock_motor import AsyncMongoMockClient
+from prp.models.species import BrackenSpeciesPrediction, MykrobeSpeciesPrediction
 
 from .data import *
 
@@ -92,3 +95,33 @@ def fastapi_client(sample_database):
     client = TestClient(app)
 
     return client
+
+
+@pytest.fixture
+def valid_qc_threshold_toml() -> str:
+    return textwrap.dedent("""
+        [species.bracken.staphylococcus_aureus]
+        min_fraction = 0.6
+        min_reads = 10000
+
+        [species.bracken.default]
+        min_fraction = 0.5
+        min_reads = 5000
+
+        [species.mykrobe.mycobacterium_tuberculosis]
+        min_species_coverage = 0.9
+        min_phylogenetic_group_coverage = 0.9
+
+        [species.mykrobe.default]
+        min_species_coverage = 0.8
+        min_phylogenetic_group_coverage = 0.8
+    """)
+
+
+@pytest.fixture
+def thresholds_file(tmp_path: Path, valid_qc_threshold_toml: str):
+    path = tmp_path / "thresholds.toml"
+    path.write_text(valid_qc_threshold_toml, encoding="utf-8")
+    # secure permissions by default
+    path.chmod(stat.S_IRUSR | stat.S_IWUSR)
+    return path
