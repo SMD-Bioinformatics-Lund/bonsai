5a54842f5c5456e21abddef648f5d9e40ef88569
Markus Johansson <markus.johansson@me.com>
2025-08-27 13:05:53 +0200

    added minhash worker for periodic tasks

--- minhash_service/minhash_service/config.py
+++ minhash_service/minhash_service/config.py
@@ -1,8 +1,11 @@
 """Configuration for minhash service"""
 
+from enum import StrEnum
 import tempfile
 from pathlib import Path
 from typing import Any
+from copy import deepcopy
+from logging import config as logging_config
 
 from pydantic import ConfigDict, DirectoryPath, Field, PositiveInt, field_validator
 from pydantic_settings import BaseSettings
@@ -11,6 +14,16 @@ from pydantic_settings import BaseSettings
 def _get_trash_dir() -> Path:
     """Create a temporary directory for trash."""
     return Path(tempfile.mkdtemp(prefix="minhash_trash_"))
+
+
+class LogLevel(StrEnum):
+    """Log level options."""
+
+    DEBUG = "DEBUG"
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+    CRITICAL = "CRITICAL"
 
 
 class MongodbConfig(BaseSettings):
@@ -35,6 +48,30 @@ class RedisConfig(BaseSettings):
     queue: str = "minhash"
 
 
+class BasePeriodicTaskConfig(BaseSettings):
+    """Generic configuration for periodic integrity check task."""
+
+    endabled: bool = True  # enable/disable background task processing
+    cron: str = "0 12 * * SAT"  # cron schedule for periodic tasks
+    queue: str = "minhash"  # redis queue name for periodic tasks
+
+
+class PeriodicIntegrityCheckConfig(BasePeriodicTaskConfig):
+    """Configure scheudling of background tasks."""
+
+    model_config = ConfigDict(env_prefix="integrity_task_")
+
+    cron: str = "0 12 * * SAT"  # cron schedule for periodic tasks
+
+
+class CleanupRemovedFilesConfig(BasePeriodicTaskConfig):
+    """Configure scheudling of background tasks."""
+
+    model_config = ConfigDict(env_prefix="purge_files_task_")
+
+    cron: str = "0 * * * *"  # cron schedule for periodic tasks
+
+
 class Settings(BaseSettings):
     """Minhash service settings."""
 
@@ -47,6 +84,11 @@ class Settings(BaseSettings):
 
     redis: RedisConfig = RedisConfig()
     mongodb: MongodbConfig = MongodbConfig()
+    # periodic tasks
+    periodic_integrity_check: PeriodicIntegrityCheckConfig = PeriodicIntegrityCheckConfig()
+    cleanup_removed_files: CleanupRemovedFilesConfig = CleanupRemovedFilesConfig()
+
+    log_level: LogLevel = LogLevel.INFO
 
     @field_validator("trash_dir", mode="before")
     @classmethod
@@ -54,6 +96,14 @@ class Settings(BaseSettings):
         """Ensure that the trash directory exists."""
         v.mkdir(parents=True, exist_ok=True)
         return v
+
+    def build_logging_conffig(self) -> dict[str, Any]:
+        """Build logging configuration dictionary."""
+        log_config = deepcopy(LOG_CONFIG)
+        # update log levels
+        log_config["handlers"]["default"]["level"] = self.log_level.value
+        log_config["loggers"]["root"]["level"] = self.log_level.value
+        return log_config
 
 
 # Logging configuration
@@ -79,6 +129,10 @@ LOG_CONFIG: dict[str, Any] = {
         },
     },
 }
+
+def configure_logging(cnf: Settings) -> None:
+    """Configure logging from settings."""
+    logging_config.dictConfig(cnf.build_logging_conffig())
 
 
 settings = Settings()
