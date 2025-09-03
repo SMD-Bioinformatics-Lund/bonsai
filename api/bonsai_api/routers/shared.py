b427d969e8731a21f9046331cfac804156f2d6e1
Markus Johansson <markus.johansson@me.com>
2025-09-03 13:47:36 +0200

    minhash.add_signature validates that input is json string

--- api/bonsai_api/routers/shared.py
+++ api/bonsai_api/routers/shared.py
@@ -1,9 +1,35 @@
 """Resources shared by many routers."""
 
-from fastapi import Path
+import gzip
+import json
+import logging
+from fastapi import HTTPException, Path, File, UploadFile
 from enum import StrEnum
 
 from ..models.sample import SAMPLE_ID_PATTERN
+
+LOG = logging.getLogger(__name__)
+
+async def parse_signature_json(signature: UploadFile = File()) -> str:
+    """Parse submitted json file that is either compressed or in clear text."""
+    head = await signature.read(2)
+    await signature.seek(0)
+    is_gzipped = head.startswith(b"\x1f\x8b")
+
+    raw = await signature.read()
+    await signature.close()
+
+    if is_gzipped:
+        try:
+            raw = gzip.decompress(raw)
+        except OSError:
+            LOG.error("Invalid gzip file: %s", signature.filename)
+            raise HTTPException(400, "Invalid gzip file")
+    try:
+        data = json.loads(raw.decode("utf-8"))
+    except Exception as err:
+        raise HTTPException(400, "File is not valid UTF-8 JSON") from err
+    return json.dumps(data)
 
 SAMPLE_ID_PATH: str = Path(
     ...,
