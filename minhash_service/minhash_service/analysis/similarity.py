9ec4d285f5ea929de96ae5b5393b306ae51e0b64
Markus Johansson <markus.johansson@me.com>
2025-09-04 12:51:02 +0200

    Similarity search lookup sample if from repo

--- minhash_service/minhash_service/analysis/similarity.py
+++ minhash_service/minhash_service/analysis/similarity.py
@@ -12,8 +12,8 @@ from .models import AniEstimateOptions
 LOG = logging.getLogger(__name__)
 
 
-def get_similar_signatures_v2(
-    query_sig: SourmashSignature, index: BaseIndexStore, 
+def get_similar_signatures(
+    query_sig: SourmashSignature, index_repo: BaseIndexStore, 
     min_similarity: float, ani_estimate: AniEstimateOptions = AniEstimateOptions.JACCARD, 
     limit: int | None = None, ignore_abundance: bool = False
 ) -> SearchResult:
@@ -47,7 +47,7 @@ def get_similar_signatures_v2(
         try:
             results = search_databases_with_abund_query(
                 query_sig,
-                [index],
+                [index_repo.index],
                 threshold=min_similarity,
                 do_containment=containment,
                 do_max_containment=max_containment,
@@ -60,7 +60,7 @@ def get_similar_signatures_v2(
     else:
         results = search_databases_with_flat_query(
             query_sig,
-            [index],
+            [index_repo.index],
             threshold=min_similarity,
             do_containment=containment,
             do_max_containment=max_containment,
@@ -69,46 +69,3 @@ def get_similar_signatures_v2(
             estimate_ani_ci=False,
         )
     return results
-
-# def get_similar_signatures(
-#     signature_file: Path, cnf: Settings, min_similarity: float, limit: int | None = None
-# ) -> SimilarSignatures:
-#     """Get find samples that are similar to reference sample.
-
-#     min_similarity - minimum similarity score to be included
-#     """
-#     LOG.info(
-#         "Finding similar: %s; similarity: %f, limit: %d",
-#         signature_file.name,
-#         min_similarity,
-#         limit,
-#     )
-
-#     # load sourmash index
-#     LOG.debug("Getting samples similar to: %s", signature_file.name)
-#     index_path = get_sbt_index(cnf)
-#     LOG.debug("Load index file to memory")
-#     db: SBT = sourmash.load_file_as_index(str(index_path))
-
-#     # load reference sequence
-#     query_signature = read_signature(signature_file, cnf)[0]
-
-#     # query for similar sequences
-#     LOG.debug("Searching for signatures with similarity > %f", min_similarity)
-#     result: list[Result] = db.search(
-#         query_signature, threshold=min_similarity
-#     )  # read sample information of similar samples
-
-#     samples: list[SimilarSignature] = []
-#     for itr_no, (similarity, sig, _) in enumerate(result, start=1):
-#         # extract sample id from sample name
-#         sid = sig.filename.split("_")[0] if sig.name == "" else sig.name
-#         signature_path = index_path.joinpath(sig.filename)
-#         LOG.info("no %d - path: %s -> %s", itr_no, signature_path, sid)
-#         samples.append(SimilarSignature(sample_id=sid, similarity=similarity))
-
-#         # break iteration if limit is reached
-#         if isinstance(limit, int) and limit == itr_no:
-#             break
-#     LOG.info("Found %d samples similar to %s", len(samples), signature_file.name)
-#     return samples
