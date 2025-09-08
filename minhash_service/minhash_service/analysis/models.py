a54d8adc408647c87d3f40728539b6fbfc9c1193
Markus Johansson <markus.johansson@me.com>
2025-09-08 13:54:45 +0200

    search_similar can take a subset of sample ids as input
    Also fixed some typehinting warnings

--- minhash_service/minhash_service/analysis/models.py
+++ minhash_service/minhash_service/analysis/models.py
@@ -2,7 +2,7 @@
 
 from enum import StrEnum
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 Signatures = list[dict[str, int | list[int]]]
 SignatureEntry = dict[str, str | Signatures]
@@ -30,6 +30,30 @@ class SimilarSignature(BaseModel):  # pydantic: disable=too-few-public-methods
 
     sample_id: str
     similarity: float
+
+
+class SimilaritySearchConfig(BaseModel):
+    """Parameters for similarity searches."""
+
+    min_similarity: float = Field(
+        ge=0,
+        le=1,
+        description="Only inlcude samples with a similarity score equal or greater than threshold.",
+    )
+    ani_estimate: AniEstimateOptions = Field(
+        default=AniEstimateOptions.JACCARD,
+        description="How sourmash should estimate average nucleotide identity (ANI).",
+    )
+    limit: int | None = Field(
+        default=None, description="Limit the search results to N hits."
+    )
+    ignore_abundance: bool = Field(
+        default=False,
+        description="If abundance information in Sourmash signature be ignored.",
+    )
+    subset_checksums: list[str] | None = Field(
+        default=None, description="Subset search to signatures with checksum."
+    )
 
 
 SimilarSignatures = list[SimilarSignature]
