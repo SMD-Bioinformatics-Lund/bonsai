b5533f4ed6281116101c5f0bc7bfcb9634557ec8
Markus Johansson <markus.johansson@me.com>
2025-09-15 10:35:14 +0200

    audit log boilerplate

---
+++ audit_log_service/audit_log/core/config.py
@@ -0,0 +1,38 @@
+"""Application settings."""
+
+from enum import StrEnum
+
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class LogLevel(StrEnum):
+    """Log level options."""
+
+    DEBUG = "DEBUG"
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+    CRITICAL = "CRITICAL"
+
+
+class MongodbConfig(BaseSettings):
+    """MongoDB configuration for minhash service."""
+
+    model_config = SettingsConfigDict(env_prefix="mongodb_")
+
+    uri: str = Field(
+        default="mongodb://mongodb:27017/", description="MongoDb connection URI."
+    )
+    database: str = Field(default="bonsai", description="The database where logs are stored.")
+    collection: str = Field(default="events", description="The collection for storing events.")
+
+
+class Settings(BaseSettings):
+    """Base app settings."""
+
+    service_name: str = "audit-log-service"
+    log_level: LogLevel = LogLevel.INFO
+    mongo: MongodbConfig = MongodbConfig()
+
+    model_config = SettingsConfigDict(env_file=".env", case_sensitive=False)
