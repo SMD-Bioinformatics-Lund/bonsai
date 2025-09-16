ccbee751061b2a8397443266ddbff2b8ee3c2d10
Markus Johansson <markus.johansson@me.com>
2025-09-16 14:29:11 +0200

    Added a few basic tests

--- audit_log_service/audit_log_service/app.py
+++ audit_log_service/audit_log_service/app.py
@@ -2,31 +2,42 @@
 
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from pymongo import MongoClient
 
-from .db import close_mongo_client, get_mongo_client
+from .db import get_mongo_connection
 from .core.config import Settings
 from .core.logging import configure_logging
 from .version import __version__
-
-settings = Settings()
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    """Instanciate the template repository and add to state."""
-    configure_logging(settings)
-    get_mongo_client(settings)
-
-    yield
-
-    close_mongo_client()
+from .api import router
 
 
-def create_app() -> FastAPI:
+def lifespan_factory(settings: Settings, injected_client: MongoClient | None = None):
+    """Create API setup and teardown functions."""
+
+    @asynccontextmanager
+    async def lifespan(api_app: FastAPI):
+        """Instanciate the template repository and add to state."""
+        configure_logging(settings)
+        client = injected_client or get_mongo_connection(settings)
+        api_app.state.database = client
+
+        try:
+            yield
+        finally:
+            if injected_client is None:
+                client.close()
+    return lifespan
+
+
+def create_app(settings: Settings, injected_client: MongoClient | None = None) -> FastAPI:
     """Create API."""
 
-    app = FastAPI(title="Audit Log Service", version=__version__, lifespan=lifespan)
+    api_app = FastAPI(
+        title="Audit Log Service", version=__version__, 
+        lifespan=lifespan_factory(settings, injected_client))
     # define routers
-    return app
+    api_app.include_router(router)
+    return api_app
 
-app = create_app()
+config = Settings()
+app = create_app(config)
