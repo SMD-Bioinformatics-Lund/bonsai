76b2769990252a59a4821aa24fd72983892b7bea
Markus Johansson <markus.johansson@me.com>
2025-09-24 18:06:06 +0200

    Use a context manager to log events in crud functions

--- api/bonsai_api/crud/utils.py
+++ api/bonsai_api/crud/utils.py
@@ -2,6 +2,13 @@
 
 from typing import Any
 from motor.motor_asyncio import AsyncIOMotorCollection
+from contextlib import contextmanager
+
+from bonsai_api.models.context import ApiRequestContext
+
+import bonsai_api
+from api_client.audit_log import AuditLogClient
+from api_client.audit_log.models import Subject, EventSeverity, EventCreate
 
 async def get_deprecated_records(collection: AsyncIOMotorCollection, schema_version: int) -> list[dict[str, Any]]:
     """Get documents from the collection that have a schema version."""
@@ -10,3 +17,34 @@ async def get_deprecated_records(collection: AsyncIOMotorCollection, schema_vers
         {"schema_version": {"$exists": False}}
     ]})
     return [dict(doc) for doc in await cursor.to_list(None)]
+
+
+@contextmanager
+def audit_event_context(
+    audit: AuditLogClient | None,
+    event_type: str,
+    ctx: ApiRequestContext,
+    subject: Subject,
+    metadata: dict[str, Any] | None = None,
+):
+    exc = None
+    try:
+        yield
+    except Exception as e:
+        exc = e
+        raise
+    finally:
+        if isinstance(audit, AuditLogClient):
+            meta = dict(metadata) if metadata else {}
+            severity = EventSeverity.ERROR if exc else EventSeverity.INFO
+            if exc:
+                meta['exception'] = str(exc)
+            event = EventCreate(
+                source_service=bonsai_api.__name__,
+                event_type=event_type,
+                severity=severity,
+                actor=ctx.actor,
+                subject=subject,
+                metadata=meta,
+            )
+            audit.post_event(event)
