02c99b27750ad52f23c48fd7c8e40cacbd1dc120
Markus Johansson <markus.johansson@me.com>
2025-09-26 16:11:36 +0200

    WIP seeding of bonsai db with seperate services

--- e2e_tests/e2e_test_entrypoint.sh
+++ e2e_tests/e2e_test_entrypoint.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# Load test samples
-# =================
-for file in /app/fixtures/samples/*.yaml; do
-  echo "Uploading sample $file"
-  /app/upload_sample.py \
-    --user     admin \
-    --password admin \
-    --api      http://api:8000/ \
-    upload-sample \
-    --input    "$file"
-done
-
 # Run container CMD
 # =================
 echo "Executing command: ${@}"
