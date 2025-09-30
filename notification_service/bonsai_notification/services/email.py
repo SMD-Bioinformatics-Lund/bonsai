ae6939b883bbdec9cf8371162a0ab38198d313a0
Markus Johansson <markus.johansson@me.com>
2025-09-30 11:17:17 +0200

    Fixed typo

--- notification_service/bonsai_notification/services/email.py
+++ notification_service/bonsai_notification/services/email.py
@@ -41,7 +41,7 @@ def send_email(
             # pass context to template
             html_content = template.render(
                 sender_name=sender_name,
-                **message_obj.model_dump(exclude=["teplate_name", "content_type"])
+                **message_obj.model_dump(exclude=set(["template_name", "content_type"]))
             )
             mail.set_content(html_content, subtype="html")
         conn.send_message(mail)
