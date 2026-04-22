cb3e985a7406000bddb3243c8c23bf46e7994c82
Markus Johansson <markus.h.johansson@skane.se>
2026-04-22 10:55:37 +0200

    Add test to ensure that identical signatures are reported

--- minhash_service/tests/analysis/test_similarity.py
+++ minhash_service/tests/analysis/test_similarity.py
@@ -6,14 +6,16 @@ import pytest
 
 from minhash_service.analysis.models import SimilaritySearchConfig
 from minhash_service.analysis.similarity import get_similar_signatures
-from minhash_service.signatures.index import SBTIndexStore
+from minhash_service.signatures.index import SBTIndexStore, RocksDBIndexStore
 from minhash_service.signatures.io import read_signatures
 
 from ..utils import get_data_path
 
 
 @pytest.mark.parametrize("limit,exp_samples", [(None, 4), (4, 4), (2, 2)])
-def test_get_similar_signatures(data_dir: Path, limit: int | None, exp_samples: int):
+def test_get_similar_signatures_no_dupl(data_dir: Path, limit: int | None, exp_samples: int):
+    """Test get similar signatures with no duplicates in the index."""
+
     cnf = SimilaritySearchConfig(min_similarity=0.5, limit=limit)
     # read query signature
     query_path = get_data_path(data_dir, "DRR237260.sig")
@@ -28,3 +30,23 @@ def test_get_similar_signatures(data_dir: Path, limit: int | None, exp_samples: 
 
     # test that limit was respected
     assert len(sigs) == exp_samples
+
+
+def test_get_similar_signatures_dupl(data_dir: Path):
+    """Test get duplicated signatures in the index."""
+
+    cnf = SimilaritySearchConfig(min_similarity=0.5)
+
+    # read query signature
+    query_path = get_data_path(data_dir, "DRR237260.sig")
+    query_sig = read_signatures(query_path, 31)
+
+    # get index
+    idx_path = get_data_path(data_dir, "rocksdb31.duplicates")
+    idx = RocksDBIndexStore(idx_path)
+
+    result = get_similar_signatures(query_sig[0], idx, config=cnf)
+
+    # assert that both the query and duplicate was found
+    matches = [r.match.name for r in result]
+    assert set(["DRR237260", "DRR237260.dupl"]).issubset(set(matches))
