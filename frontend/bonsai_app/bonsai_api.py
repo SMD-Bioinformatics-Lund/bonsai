f1d4918c90d9bd3e8589b6b4b65c4a7f6ee6b433
Markus Johansson <markus.h.johansson@skane.se>
2026-06-23 13:41:10 +0200

    Login uses API client

--- frontend/bonsai_app/bonsai_api.py
+++ frontend/bonsai_app/bonsai_api.py
@@ -2,7 +2,6 @@
 
 import logging
 
-import requests
 from bonsai_libs.api_client.bonsai import BonsaiApiClient
 from bonsai_libs.api_client.core import BearerTokenAuth
 from flask import current_app, g
@@ -12,6 +11,8 @@ LOG = logging.getLogger(__name__)
 
 
 def get_api_client():
+    """Get API client instance using token from logged in user."""
+
     if "api_client" not in g:
         # get token from current user
         token = current_user.token.access_token
@@ -21,31 +22,3 @@ def get_api_client():
             auth=BearerTokenAuth(token),
         )
     return g.api_client
-
-
-def fetch_user_data(base_url: str, token: str) -> dict | None:
-    headers = {"Authorization": f"Bearer {token}"}
-
-    resp = requests.get(
-        f"{base_url}/users/me",
-        headers=headers,
-        timeout=5,
-    )
-
-    if resp.status_code != 200:
-        return None
-
-    return resp.json()
-
-
-def authenticate_user(base_url: str, username: str, password: str) -> dict[str, str]:
-
-    resp = requests.post(
-        f"{base_url}/token",
-        data={"username": username, "password": password},
-        timeout=5,
-    )
-
-    if resp.status_code != 200:
-        raise ValueError("Login failed: {resp.status_code}")
-    return resp.json()
